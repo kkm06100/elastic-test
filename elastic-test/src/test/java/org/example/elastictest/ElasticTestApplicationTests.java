5eed3e2e542a139cfd020e7ed8cf1e213391243f
김경민 <kkm07133@naver.com>
2025-06-17 11:14:12 +0900

    프로젝트 시작

---
+++ elastic-test/src/test/java/org/example/elastictest/ElasticTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.elastictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
